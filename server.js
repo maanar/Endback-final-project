0ab4521d1ef290d82072859436bdd2c1d55c730a
Youssef Ashraf <useefashraf22@gmail.com>
2022-12-18 23:05:53 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// to run : npm run server
+const express = require('express');
+
+//const connectDB = require('./config/db');
+
+const app = express();
+
+//Connect to database
+//connectDB();
+
+//init middleware
+// app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('Welcome FRAISCH WEBSITE ☺♥'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
