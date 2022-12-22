1058538dc8332b6374ba7027f0fe2a3ed535a042
manar ashraf <ashrafmanar151@gmail.com>
2022-12-22 17:15:15 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 // to run : npm run server
+//git commit -m 'Initial commit'
 
 const express = require('express');
 
 const connectDB = require('./config/db');
 
 const app = express();
- 
 
 //Connect to database
 connectDB();
 
 //init middleware // it allow us to get the data in request stop body
- app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Welcome FRAISCH WEBSITE ☺♥'));
 
-
-//define routes to make requist 
+//define routes to make requist
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
