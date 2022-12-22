c5aa6c919279f9d72426c583c573d3ae07517c49
Youssef Ashraf <useefashraf22@gmail.com>
2022-12-22 17:02:29 +0200

    Initial commit

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 // to run : npm run server
+//git commit -m 'Initial commit'
 
 const express = require('express');
 
 const connectDB = require('./config/db');
 
 const app = express();
- 
 
 //Connect to database
 connectDB();
 
 //init middleware // it allow us to get the data in request stop body
- app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Welcome FRAISCH WEBSITE ☺♥'));
 
-
-//define routes to make requist 
+//define routes to make requist
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
