fadbc66da411547020ac6dc92836c636337771d4
Youssef Ashraf <120688787+YoussefAshraf22@users.noreply.github.com>
2022-12-24 16:22:36 +0200

    Request & body validation for user

---
+++ models/User.js
@@ -0,0 +1,28 @@
+//Schema which just holds the different fiels that we want
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    // it allows u to attach a profile image to ur email , accssible
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+});
+module.exports = User = mongoose.model('user', UserSchema);
