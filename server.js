761d1f36531ea9e9e7ee6e3f28d137f5393b3596
maanar <120127709+maanar@users.noreply.github.com>
2022-12-28 23:12:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,35 @@ const express = require('express');
 const connectDB = require('./config/db');
 
 const app = express();
+const mongoose = require('mongoose');
+const path = require("path");
 
+
+//routes
+/*
+const authRoutes =require('./routes/api/auth');
+const productRoutes =require('./routes/api/product');
+const profileRoutes = require('./routes/api/profile');
+const userRoutes =require('./routes/api/users');
+*/
 //Connect to database
 connectDB();
 
 //init middleware // it allow us to get the data in request stop body
 app.use(express.json({ extended: false }));
+app.use("/public", express.static(path.join(__dirname, "uploads")))
 
 app.get('/', (req, res) => res.send('Welcome FRAISCH WEBSITE ☺♥'));
+
+
+/*
+app.use('/users', userRoutes);
+app.use('/auth',  authRoutes);
+app.use('/profile', profileRoutes);  
+app.use('/product' , productRoutes);
+
+*/
+
 
 //define routes to make requist
 app.use('/api/users', require('./routes/api/users'));
